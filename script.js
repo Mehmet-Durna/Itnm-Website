33cfd52ef2c58d6191f0525b4bc869fdd79ea956
Mehmet-Durna <r0900513@student.thomasmore.be>
2022-04-04 15:08:50 +0200

    overflow problem

--- script.js
+++ script.js
@@ -21,29 +21,6 @@ let nCount = selector => {
 };
 
 
-
-
-
-/**
- *
- *  sticky navigation
- *
- */
-
-let navbar = $(".navbar");
-
-$(window).scroll(function () {
-  // get the complete hight of window
-  let oTop = $(".section-2").offset().top - window.innerHeight;
-  if ($(window).scrollTop() > oTop) {
-    navbar.addClass("sticky");
-  } else {
-    navbar.removeClass("sticky");
-  }
-});
-
-
-
 /**
  *
  *  landing slider
